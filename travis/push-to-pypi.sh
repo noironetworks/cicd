3dd124d3c31b1dc091f2a332668235240032309c
snaiksat <sumitnaiksatam@gmail.com>
2023-06-10 03:57:13 -0400

    Use twine for test pypi upload

--- travis/push-to-pypi.sh
+++ travis/push-to-pypi.sh
@@ -3,17 +3,6 @@ set -x
 SCRIPTS_DIR=$(dirname ${BASH_SOURCE[0]})
 source "$SCRIPTS_DIR/globals.sh"
 
-cat >~/.pypirc <<EOL
-[distutils]
-  index-servers =
-    test-pypi
-
-[test-pypi]
-  repository: https://test.pypi.org/legacy/
-  username: ashuparu
-  password: ${PYPI_PASS}
-EOL
-cat ~/.pypirc
 cd provision
 python setup.py --description
 #python setup.py sdist upload -r test-pypi
