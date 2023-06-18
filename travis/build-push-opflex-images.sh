d7d5d13c74deef6c985415acb19473cc85876473
snaiksat <sumitnaiksatam@gmail.com>
2023-06-18 11:01:48 -0400

    Remove redundant opflex-build-base push

--- travis/build-push-opflex-images.sh
+++ travis/build-push-opflex-images.sh
@@ -17,8 +17,3 @@ ALL_IMAGES="opflex-build-base opflex-build opflex"
 for IMAGE in ${ALL_IMAGES}; do
   $SCRIPTS_DIR/push-images.sh ${IMAGE_BUILD_REGISTRY} ${IMAGE} ${IMAGE_BUILD_TAG} ${OTHER_IMAGE_TAGS}
 done
-
-# Revisit: We do the following so that we have an updated opflex-build-base image available in
-# quay for aci-containers-host container builds
-docker tag ${IMAGE_BUILD_REGISTRY}/opflex-build-base:${IMAGE_BUILD_TAG} ${IMAGE_BUILD_REGISTRY}/opflex-build-base:${RELEASE_TAG_WITH_UPSTREAM_ID}
-docker push ${IMAGE_BUILD_REGISTRY}/opflex-build-base:${RELEASE_TAG_WITH_UPSTREAM_ID}
